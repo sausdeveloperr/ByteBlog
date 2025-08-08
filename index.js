bd55f21924a0cb648b594cf285e8e3224d7e39db
Lekan <salamiolamilekan8991@gmail.com>
2025-08-08 18:32:56 +0100

    Add home page markup and other necessary resources

--- index.js
+++ index.js
@@ -2,3 +2,18 @@
 git commit -m "msg"
 git push -u origin main */
 
+import express from 'express';
+
+const app = express();
+const port = 9090;
+
+app.use(express.urlencoded({extended: true}))
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.send('ByteBlog is up and running')
+})
+
+app.listen(port, () => {
+    console.log('server running at port', port)
+})
