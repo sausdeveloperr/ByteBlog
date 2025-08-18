172b041a98150588330932eca95d9538bf593b2e
Lekan <salamiolamilekan8991@gmail.com>
2025-08-18 22:20:11 +0100

    Organize markup, Create article view template for individual posts

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 /* git add .
 git commit -m "msg"
 git push -u origin main */
+// git commit --amend -m "New message" - amend the last commit you just dropped
 
 import express from 'express';
 
@@ -10,12 +11,19 @@ const port = 4789;
 app.use(express.urlencoded({extended: true}))
 app.use(express.static('public'));
 
+// const currentYear = new Date().getFullYear();
 app.get('/', (req, res) => {
-    // const currentYear = new Date().getFullYear();
-    res.render('index.ejs');
+    res.render('home.ejs');
+})
+
+app.get('/post', (req, res) => {
+    res.render('article.ejs');
+})
+
+app.get('/copy', (req, res) => {
+    res.render('bookmark.ejs');
 })
 
 app.listen(port, () => {
     console.log('server running at port', port)
 })
-// git commit --amend -m "New message" - amend the last commit you just dropped
