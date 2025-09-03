7af5f63d35f59eeeb00ff2ff52d660a8f849ef6d
Lekan <salamiolamilekan8991@gmail.com>
2025-09-03 20:17:21 +0100

    testing things out

--- app.js
+++ app.js
@@ -4,14 +4,14 @@ git push -u origin main */
 // git commit --amend -m "New message" - amend the last commit dropped
 
 import express from 'express';
-// import seedPosts from './data/seedPosts'; This
-// import { seedPosts } from './data/seedPosts'; or That?
+import seedPosts from './data/seedPosts';
 
 const app = express();
 const port = 4789;
 
 app.use(express.urlencoded({extended: true}))
 app.use(express.static('public'));
+app.set('view engine', 'ejs');
 
 
 app.get('/', (req, res) => {
@@ -19,8 +19,8 @@ app.get('/', (req, res) => {
 })
 
 app.get('/view-article', (req, res) => {
-    // const mockPosts = seedPosts;
-
+    const mockPosts = seedPosts;
+    console.log(mockPosts);
     res.render('view-article.ejs');
 })
 
